27eedea1156638ca534ac0cc6bdfd0b801384693
KutkinRoman <kutkin.roman.i@gmail.com>
2023-01-29 18:00:18 +0600

    books form

--- src/components/book/BookForm.tsx
+++ src/components/book/BookForm.tsx
@@ -4,6 +4,7 @@ import TextFiled from "../form/TextFiled";
 import Select from "../form/Select";
 import {observer} from "mobx-react-lite";
 import {useAppStore} from "../../context/useAppStore";
+import {useFieldArray, useForm} from "react-hook-form";
 
 const BookForm = () => {
     const bookStore = useAppStore().bookStore
@@ -12,17 +13,37 @@ const BookForm = () => {
         bookStore.initCategories()
     }, [])
 
+    const {register, control, handleSubmit, reset, trigger, setError} = useForm({});
+
+    const {fields, append, remove} = useFieldArray({
+        control,
+        name: "categories",
+    });
+
+    useEffect(() => {
+        append(null)
+    }, [])
+
+    const onSubmit = (date: any) => {
+        console.log(date)
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <TextFiled
                 label={'Наименование'}
             />
-            <Select
-                label={'Категория'}
-                items={bookStore.categories}
-                getLabelItem={item => item.name}
-            />
+            {fields.map((field, index) => (
+                <Select
+                    key={field.id}
+                    {...register(`categories.${index}.value`)}
+                    label={'Категория'}
+                    items={bookStore.categories}
+                    getLabelItem={item => item.name}
+                />
+            ))}
             <Button
+                type={'submit'}
                 children={'Сохранить'}
             />
         </form>
